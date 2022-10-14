91874712cc80695831696337e557b7f76cd1e890
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-14 14:46:15 -0400

    categorias

--- src/GIFExpertApp.jsx
+++ src/GIFExpertApp.jsx
@@ -1,9 +1,29 @@
 
+import { useState } from "react"
 
 export const GifExpertApp = () => {
-    return (
-        <>
+    const [categories, setCategories] = useState(['One punch','Narurto'])
+    return(      <>
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* input  */}
+        {/* listado de GIf */}
+        <ol>
+            {categories.map( category => {
+                return <li key={category}>{category}</li>
+            })}
+            
+            
+        </ol>
+        {/* Gif item */}
+
+
+        )  
+
+
+
+
         </>
        
     )
