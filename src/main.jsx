be7ed13005ec394f752775d32aa848c48b493209
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-14 14:30:51 -0400

    estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  {GifExpertApp}  from './GIFExpertApp';
+import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
