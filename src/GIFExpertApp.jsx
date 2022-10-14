0cbb2b3708180aded662d2b69863bd23dfe025d6
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-14 14:57:57 -0400

    categorias

--- src/GIFExpertApp.jsx
+++ src/GIFExpertApp.jsx
@@ -3,27 +3,25 @@ import { useState } from "react"
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One punch','Narurto'])
+    const onAddCategory = ()=>{
+        setCategories(cat =>[...cat,'Isagi'])
+    }
     return(      <>
         {/* titulo */}
         <h1>GifExpertApp</h1>
 
         {/* input  */}
         {/* listado de GIf */}
+        <button onClick={onAddCategory}>Agregar</button>
         <ol>
             {categories.map( category => {
                 return <li key={category}>{category}</li>
             })}
-            
-            
+               
         </ol>
         {/* Gif item */}
 
-
         )  
-
-
-
-
         </>
        
     )
