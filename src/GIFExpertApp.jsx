e99ea7312e9139c74c467e97987c07223edb9f84
FrancisHenriquez <francishenriquez07@gmail.com>
2022-10-17 10:59:08 -0400

    comunicacion entre componentes

--- src/GIFExpertApp.jsx
+++ src/GIFExpertApp.jsx
@@ -6,18 +6,18 @@ import { AddCategory } from "./components/AddCategory"
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One punch','Narurto'])
     const onAddCategory1 = ()=>{
-        setCategories(cat =>[...cat,'Isagi'])
+        // setCategories(cat =>[...cat,'Isagi'])
     }
     return(      <>
         {/* titulo */}
         <h1>GifExpertApp</h1>
 
-        <AddCategory/>
+        <AddCategory setCategories = {setCategories}/>
         <input type="text" />
 
         {/* input  */}
         {/* listado de GIf */}
-        <button onClick={onAddCategory1}>Agregar</button>
+        {/* <button onClick={onAddCategory1}>Agregar</button> */}
         <ol>
             {categories.map( category => {
                 return <li key={category}>{category}</li>
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,18 +1,29 @@
 
 import { useState } from "react"
-export const AddCategory = () => {
+
+
+export const AddCategory = ({ setCategories }) => {
 
     const [InputValue, setInputValue] = useState('Blue Lock')
-    const onInputChange = ({ target })=> {
-        setInputValue(target.value)
+
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value )
     }
+
+    const onSubmit = (event) => {
+      event.preventDefault();
+      if ( InputValue.trim().length <= 1 ) return;
+      setCategories( categories => [ InputValue, ...categories ]);
+      setInputValue( ' ' );
+    }
+
   return (
     <form onSubmit={(event) => onSubmit(event)} >
-    <input 
-    type="text"
-    placeholder="Buscar Gifs" 
-    value = {InputValue}
-    onChange ={ onInputChange  }  
+       <input 
+           type="text"
+            placeholder="Buscar Gifs" 
+            value = {InputValue}
+             onChange ={ onInputChange  }  
     />
     </form>
   )
